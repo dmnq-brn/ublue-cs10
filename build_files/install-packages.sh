2faacd12b64260a025c6ca4a6f9d5de98dac49d0
dmnq-brn <dominique.baron@cs-soprasteria.com>
2026-09-18 19:00:05 +0200

    Update install-packages.sh

--- build_files/install-packages.sh
+++ build_files/install-packages.sh
@@ -4,46 +4,48 @@ set -ouex pipefail
 
 # Base packages from repos - common to all versions
 SHARED_PACKAGES=(
-    # Gnome minimal desktop
-    NetworkManager-wifi
     # PackageKit-command-not-found
     # PackageKit-gtk3-module
     audit
     bootc
     # bpftool
-    dconf
     dnsmasq
-    # distrobox not part of centos packages
     firewalld
+    plymouth
+    plymouth-system-theme
+    polkit
+    rsync
+    realmd
+    smartmontools
+    vim-enhanced
+)
+
+GNOME_DESKTOP_PACKAGES=(
+    # Gnome minimal desktop
+    # ModemManager-glib required by gnome-control-center
+    dconf
     fprintd-pam
-    git-core
-    git-core-doc
+    flatpak
     gdm
     glibc-all-langpacks
-    gnome-bluetooth
     gnome-control-center
     gnome-disk-utility
-    gnome-initial-setup
+#    gnome-initial-setup
     gnome-session-wayland-session
     gnome-settings-daemon
     gnome-shell
     # gnome-shell-extension-background-logo
     gnome-software
     # gvfs-fuse
-    iw
-    iwlwifi-dvm-firmware
-    iwlwifi-mvm-firmware
+    # gweather-locations
+    # gweather-locations-common
     mesa-dri-drivers
     mesa-vulkan-drivers
     nautilus
     # orca
-    plymouth
-    plymouth-system-theme
-    polkit
     ptyxis
-    rsync
-    realmd
-    smartmontools
+    # required by Nautilus
+    # totem-pl-parser
     tracker
     tracker-miners
     xdg-desktop-portal
@@ -51,7 +53,51 @@ SHARED_PACKAGES=(
     xdg-desktop-portal-gtk
     xdg-user-dirs-gtk
     # yelp-tools
-    vim-enhanced
+)
+
+DEVELOPMENT_PACKAGES=(
+    # toolbox
+    distrobox
+    git-core
+    git-core-doc
+)
+
+QEMU_PACKAGES=(
+    qemu-user-static
+    qemu-user-static-aarch64
+    qemu-user-static-alpha
+    qemu-user-static-arm
+    qemu-user-static-hexagon
+    qemu-user-static-hppa
+    qemu-user-static-loongarch64
+    qemu-user-static-m68k
+    qemu-user-static-microblaze
+    qemu-user-static-mips
+    qemu-user-static-or1k
+    qemu-user-static-ppc
+    qemu-user-static-riscv
+    qemu-user-static-s390x
+    qemu-user-static-sh4
+    qemu-user-static-sparc
+    qemu-user-static-x86
+    qemu-user-static-xtensa
+)
+
+WIFI_PACKAGES=(
+    NetworkManager-wifi
+    iw
+    iwlwifi-dvm-firmware
+    iwlwifi-mvm-firmware
+)
+
+BLUETOOTH_PACKAGES=(
+    NetworkManager-bluetooth
+    bluez
+)
+
+GNOME_BLUETOOTH_PACKAGES=(
+    gnome-bluetooth
+    # gnome-bluetooth-libs
 )
 
 # centos specific packages
@@ -60,53 +106,43 @@ CENTOS_PACKAGES=(
 )
 
 FEDORA_PACKAGES=(
-
+    desktop-backgrounds-gnome
 )
 
-FONTS_PACKAGES=(
-    default-fonts-cjk-mono
-    default-fonts-cjk-sans
-    default-fonts-cjk-serif
-    default-fonts-core-emoji
-    default-fonts-core-math 
-    default-fonts-core-mono
-    default-fonts-core-sans
-    default-fonts-core-serif
-    default-fonts-other-mono
-    default-fonts-other-sans
-    default-fonts-other-serif
-    dejavu-sans-fonts
-    dejavu-sans-mono-fonts
-    dejavu-serif-fonts
-    google-carlito-fonts
-    google-crosextra-caladea-fonts
-    google-droid-sans-fonts
-    google-droid-sans-mono-fonts
-    google-droid-serif-fonts
-    google-noto-emoji-fonts
-    google-noto-fonts-all
-    google-noto-sans-cjk-fonts
-    google-roboto-slab-fonts pt-sans-fonts
-    redhat-display-vf-fonts
-    redhat-mono-vf-fonts
-    redhat-text-vf-fonts    
+OPENSSH_SERVER_PACKAGES=(
+    openssh-server
+)
+
+OPENSSH_CLIENT_PACKAGES=(
+    openssh-server
+)
+
+UNVANTED_PACKAGES=(
+    NetworkManager-adsl
+    NetworkManager-cloud-setup
+    NetworkManager-ovs
+    NetworkManager-ppp
+    NetworkManager-team
+    NetworkManager-wwan
 )
 
 # Guest Desktop Agents
-GUEST_DESKTOP_AGENTS=(
+GUEST_AGENTS_PACKAGES=(
     hyperv-daemons
 #    open-vm-tools-desktop
 #    qemu-guest-agent
 #    spice-vdagent
 )
 
-# Install packages
-#if [[ "${#SHARED_PACKAGES[@]}" -gt 0 ]]; then
-#    readarray -t UINSTALLED < <(rpm -qa --queryformat='%{NAME}\n' "${SHARED_PACKAGES[@]}" "${GUEST_DESKTOP_AGENTS[@]}" "${FONTS_PACKAGES[@]}" 2>/dev/null || true)
-#    if [[ "${#UINSTALLED[@]}" -gt 0 ]]; then
-        echo "Installing ${SHARED_PACKAGES[@]} ${GUEST_DESKTOP_AGENTS[@]} ${FONTS_PACKAGES[@]} packages from repos..."
-        dnf -y --setopt=install_weak_deps=False install "${SHARED_PACKAGES[@]}" "${GUEST_DESKTOP_AGENTS[@]}" "${FONTS_PACKAGES[@]}"
-#    else
-#        echo "No excluded packages found to remove."
-#    fi
-#fi
+# Install required packages
+dnf -y --setopt=install_weak_deps=False install "${SHARED_PACKAGES[@]}" "${GUEST_AGENTS_PACKAGES[@]}" "${OPENSSH_SERVER_PACKAGES[@]}"
+
+# remove unwanted packages
+readarray -t INSTALLED < <(rpm -qa --queryformat='%{NAME}\n' "${UNVANTED_PACKAGES[@]}" "${GNOME_DESKTOP_PACKAGES[@]}" "${BLUETOOTH_PACKAGES[@]}" "${GNOME_BLUETOOTH_PACKAGES[@]}" "${WIFI_PACKAGES[@]}" "${OPENSSH_CLIENT_PACKAGES[@]}" "${QEMU_PACKAGES[@]}" "${DEVELOPMENT_PACKAGES[@]}" 2>/dev/null || true)
+if [[ "${#INSTALLED[@]}" -gt 0 ]]; then
+    dnf -y remove "${INSTALLED[@]}"
+else
+    echo "No excluded packages found to remove."
+fi
+
+dnf -y upgrade

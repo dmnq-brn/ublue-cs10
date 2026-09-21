e8082ac9ef3fad7528dc25d014a54b2b7dc45fe6
dmnq-brn <dominique.baron@cs-soprasteria.com>
2026-09-21 13:12:55 +0200

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,13 +9,13 @@ cp -avf "/ctx/system_files"/. /
 /ctx/install-packages.sh
 
 ### add flatpak flathub remote repository
-flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+#flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
 ### install flatpack app
-flatpak install -y --noninteractive --system flathub com.mattjakeman.ExtensionManager
-flatpak install -y --noninteractive --system flathub page.tesk.Refine
-flatpak install -y --noninteractive --system flathub net.nokyan.Resources
-flatpak install -y --noninteractive --system org.mozilla.firefox
+#flatpak install -y --noninteractive --system flathub com.mattjakeman.ExtensionManager
+#flatpak install -y --noninteractive --system flathub page.tesk.Refine
+#flatpak install -y --noninteractive --system flathub net.nokyan.Resources
+#flatpak install -y --noninteractive --system org.mozilla.firefox
 
 ### remove gnome-initial-setup defaults configuration
 rm /usr/share/dconf/profile/gnome-initial-setup
